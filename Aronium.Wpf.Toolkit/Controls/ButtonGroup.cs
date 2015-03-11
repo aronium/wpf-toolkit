f04cf37c0cc88eb8ecb691e1cef1e32a1c36ce6f
Bosko Kukanjac <bosko.kukanjac@gmail.com>
2015-03-12 00:12:36 +0100

    .

--- Aronium.Wpf.Toolkit/Controls/ButtonGroup.cs
+++ Aronium.Wpf.Toolkit/Controls/ButtonGroup.cs
@@ -9,16 +9,20 @@ namespace Aronium.Wpf.Toolkit.Controls
 {
     public class ButtonGroup : DockPanel
     {
+        #region - Dependency properties -
         /// <summary>
         /// Identifies element offset property.
         /// </summary>
-        public static DependencyProperty ElementOffsetProperty = DependencyProperty.Register("ElementOffset", typeof(Thickness), typeof(ButtonGroup), new PropertyMetadata(new Thickness(-1, 0, 0, 0)));
+        public static DependencyProperty ElementOffsetProperty = DependencyProperty.Register("ElementOffset", typeof(Thickness), typeof(ButtonGroup), new PropertyMetadata(new Thickness(-1, 0, 0, 0))); 
+        #endregion
+
+        #region - Private methods -
 
         /// <summary>
-        /// Handle visual children being added or removed
+        /// Handle visual children being added or removed.
         /// </summary>
-        /// <param name="visualAdded">Visual child added</param>
-        /// <param name="visualRemoved">Visual child removed</param>
+        /// <param name="visualAdded">Visual child added.</param>
+        /// <param name="visualRemoved">Visual child removed.</param>
         protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
         {
             // Track when objects are added and removed
@@ -29,7 +33,18 @@ namespace Aronium.Wpf.Toolkit.Controls
 
             if (visualRemoved != null)
             {
-                // Do stuff with the removed object
+                if (this.Children.Count > 0)
+                {
+                    if (this.Children[0] is Button)
+                    {
+                        ((Button)this.Children[0]).Margin = new Thickness(0);
+                    }
+
+                    for (int i = 1; i < this.Children.Count; i++)
+                    {
+                        SetOffset(visualAdded);
+                    }
+                }
             }
 
             // Call base function
@@ -42,9 +57,11 @@ namespace Aronium.Wpf.Toolkit.Controls
             {
                 ((Button)element).Margin = this.ElementOffset;
             }
-        }
+        } 
 
+        #endregion
 
+        #region - Properties -
         /// <summary>
         /// Gets or sets element offset property.
         /// </summary>
@@ -52,11 +69,16 @@ namespace Aronium.Wpf.Toolkit.Controls
         {
             get { return (Thickness)GetValue(ElementOffsetProperty); }
             set { SetValue(ElementOffsetProperty, value); }
-        }
+        } 
+        #endregion
+
+        #region - Private properties -
 
         private bool IsEmptyMargin(Button button)
         {
             return button.Margin.Left == 0 && button.Margin.Right == 0 && button.Margin.Top == 0 && button.Margin.Bottom == 0;
-        }
+        } 
+
+        #endregion
     }
 }
