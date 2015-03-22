349f46b0e28483d4ff53046f92ba12ecd08a0c81
Bosko Kukanjac <bosko.kukanjac@ghcpro.com>
2015-03-22 14:00:34 +0100

    Converters

---
+++ Aronium.Wpf.Toolkit/Converters/UppercaseConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Aronium.Wpf.Toolkit.Converters
+{
+    [ValueConversion(typeof(object), typeof(string))]
+    public class UpperCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value != null ? value.ToString().ToUpper() : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
