1edd87a99fae12bb4942ae0e4e9ac19df82524c1
Bosko Kukanjac <bosko.kukanjac@ghcpro.com>
2015-04-10 21:01:06 +0200

    Added InvertBooleanConverter

---
+++ Aronium.Wpf.Toolkit/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Aronium.Wpf.Toolkit.Converters
+{
+    /// <summary>
+    /// Conntains helper methods for converting boolean values to oposite.
+    /// </summary>
+    public class InvertBooleanConverter : IValueConverter
+    {
+        #region - IValueConverter Members -
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return !((bool)value);
+            }
+
+            return false;
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        #endregion
+    } 
+}
