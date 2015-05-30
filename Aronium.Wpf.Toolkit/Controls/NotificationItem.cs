55149f2e323c85c4c9d411f470ae747b8362c17f
Bosko Kukanjac <bosko.kukanjac@ghcpro.com>
2015-05-30 02:28:02 +0200

    Notification control - toast messages

---
+++ Aronium.Wpf.Toolkit/Controls/NotificationItem.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace Aronium.Wpf.Toolkit.Controls
+{
+    [TemplatePart(Name = "PART_CloseButton", Type = typeof(Button))]
+    public class NotificationItem : Control
+    {
+        public static DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(object), typeof(NotificationItem));
+        public static DependencyProperty ClickToCloseProperty = DependencyProperty.Register("ClickToClose", typeof(bool), typeof(NotificationItem));
+        public static DependencyProperty ShowCloseProperty = DependencyProperty.Register("ShowClose", typeof(bool), typeof(NotificationItem), new PropertyMetadata(true));
+
+        DispatcherTimer timer;
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseDown(e);
+
+            if (this.ClickToClose)
+                this.Close();
+        }
+
+        public object Content
+        {
+            get
+            {
+                return GetValue(ContentProperty);
+            }
+            set
+            {
+                SetValue(ContentProperty, value);
+            }
+        }
+
+        public bool ClickToClose
+        {
+            get { return (bool)GetValue(ClickToCloseProperty); }
+            set { SetValue(ClickToCloseProperty, value); }
+        }
+
+        public bool ShowClose
+        {
+            get { return (bool)GetValue(ShowCloseProperty); }
+            set { SetValue(ShowCloseProperty, value); }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var closeBtn = this.Template.FindName("PART_CloseButton", this) as Button;
+
+            if (closeBtn != null)
+            {
+                closeBtn.Click += (sender, e) =>
+                {
+                    this.Close();
+                };
+            }
+        }
+
+        private void Close()
+        {
+            if (timer != null)
+            {
+                timer.Tick -= OnTimerTick;
+                timer.Stop();
+                timer.IsEnabled = false;
+            }
+
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            EventHandler fadeOutAnimationCompletedHandler = null;
+            EventHandler sizeAnimationCompletedHandler = null;
+
+            var fadeOutAnimation = new DoubleAnimation()
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromMilliseconds(200)
+            };
+
+            var sizeAnimation = new DoubleAnimation()
+            {
+                From = this.ActualHeight,
+                To = 0,
+                Duration = TimeSpan.FromMilliseconds(200),
+                BeginTime = TimeSpan.FromMilliseconds(100)
+            };
+
+            fadeOutAnimationCompletedHandler = (s, ea) =>
+            {
+                fadeOutAnimation.Completed -= fadeOutAnimationCompletedHandler;
+                this.BeginAnimation(HeightProperty, sizeAnimation);
+            };
+
+            sizeAnimationCompletedHandler = (s, ea) =>
+            {
+                sizeAnimation.Completed -= sizeAnimationCompletedHandler;
+
+                this.Close();
+            };
+
+            fadeOutAnimation.Completed += fadeOutAnimationCompletedHandler;
+            sizeAnimation.Completed += sizeAnimationCompletedHandler;
+
+            this.BeginAnimation(OpacityProperty, fadeOutAnimation);
+        }
+
+        internal void RunAutoHideTimer(int duration)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += OnTimerTick;
+            timer.Interval = TimeSpan.FromSeconds(duration);
+            timer.Start();
+        }
+    }
+}
---
+++ Aronium.Wpf.Toolkit/Controls/NotificationsControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Aronium.Wpf.Toolkit.Controls
+{
+    public class NotificationsControl : ItemsControl
+    {
+        public static DependencyProperty DurationProperty = DependencyProperty.Register("Duration", typeof(int), typeof(NotificationsControl), new PropertyMetadata(10));
+        public static DependencyProperty ClickToCloseProperty = DependencyProperty.Register("ClickToClose", typeof(bool), typeof(NotificationsControl));
+
+        public static readonly RoutedEvent ItemClosedEvent = EventManager.RegisterRoutedEvent("ItemClosed", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(NotificationsControl));
+        
+        /// <summary>
+        /// Occurs when item is closed.
+        /// </summary>
+        public event RoutedEventHandler ItemClosed
+        {
+            add { AddHandler(ItemClosedEvent, value); }
+            remove { RemoveHandler(ItemClosedEvent, value); }
+        }
+
+        public int Duration
+        {
+            get { return (int)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        public bool ClickToClose
+        {
+            get { return (bool)GetValue(ClickToCloseProperty); }
+            set { SetValue(ClickToCloseProperty, value); }
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            base.PrepareContainerForItemOverride(element, item);
+
+            NotificationItem notificationItem = element as NotificationItem;
+
+            if (!Object.ReferenceEquals(element, item) && notificationItem != null)
+            {
+                notificationItem.Content = item;
+                if (Duration > 0)
+                    notificationItem.RunAutoHideTimer(Duration);
+                
+                notificationItem.IsVisibleChanged += OnNotificationItemIsVisibleChanged;
+            }
+        }
+
+        void OnNotificationItemIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!((bool)e.NewValue))
+            {
+                this.RaiseEvent(new RoutedEventArgs(ItemClosedEvent, sender));
+            }
+        }
+
+        /// <summary>
+        /// Overriden GetContainerForItemOverride method.
+        /// </summary>
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new NotificationItem();
+        }
+
+        /// <summary>
+        /// Overriden IsItemItsOwnContainerOverride method.
+        /// </summary>
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is NotificationItem;
+        }
+    }
+}
