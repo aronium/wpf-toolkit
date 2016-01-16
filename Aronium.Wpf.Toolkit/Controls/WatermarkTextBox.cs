8a27b10acb022fc6901302de257c3a8f82e6504d
Aronium <bosko.kukanjac@ghcpro.com>
2016-01-16 18:17:15 +0100

    WatermarkTextBox - Check watermark on load

--- Aronium.Wpf.Toolkit/Controls/WatermarkTextBox.cs
+++ Aronium.Wpf.Toolkit/Controls/WatermarkTextBox.cs
@@ -14,13 +14,33 @@ namespace Aronium.Wpf.Toolkit.Controls
 
         public static DependencyProperty WatermarkProperty = DependencyProperty.Register("Watermark", typeof(string), typeof(WatermarkTextBox));
 
+        public WatermarkTextBox()
+        {
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnLoaded;
+
+            // In case Text property is bound, make sure watermark text is not visible on load.
+            // TextChanged event will toggle watermark text visibility, ensure it is set on load, too.
+            if(!string.IsNullOrEmpty(Text))
+                ToggleWatermarkTextBlock();
+        }
+
         protected override void OnTextChanged(TextChangedEventArgs e)
         {
             base.OnTextChanged(e);
 
+            ToggleWatermarkTextBlock();
+        }
+
+        private void ToggleWatermarkTextBlock()
+        {
             if (watermarkTextBlock != null)
             {
-                watermarkTextBlock.Visibility = string.IsNullOrEmpty(this.Text) ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+                watermarkTextBlock.Visibility = string.IsNullOrEmpty(this.Text) ? Visibility.Visible : Visibility.Collapsed;
             }
         }
 
