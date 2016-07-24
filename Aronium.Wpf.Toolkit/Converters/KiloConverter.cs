9a012093626a3e11f051ffe53aecb365668eaec6
Aronium <bosko.kukanjac@ghcpro.com>
2016-07-24 16:47:14 +0200

    KiloConverter added

---
+++ Aronium.Wpf.Toolkit/Converters/KiloConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Aronium.Wpf.Toolkit.Converters
+{
+    public class KiloConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                var val = System.Convert.ToDouble(value);
+
+                if (val > 1000)
+                {
+                    return (val / 1000).ToString("0.0#K");
+                }
+
+                return val.ToString("0.00");
+            }
+            catch
+            {
+                return Double.NaN;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
