86c645604ecb6fe37e1ca2ecf345f95537a1a228
Aronium <bosko.kukanjac@ghcpro.com>
2017-01-18 21:21:35 +0100

    .

---
+++ Aronium.Wpf.Toolkit/Controls/TagItem.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Aronium.Wpf.Toolkit.Controls
+{
+    public class TagItem : Control
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string), typeof(TagItem));
+        public static readonly DependencyProperty BackgroundSelectedProperty = DependencyProperty.Register("BackgroundSelected", typeof(Brush), typeof(TagItem));
+
+        public TagItem()
+        {
+        }
+
+        public TagItem(string value)
+        {
+            this.Value = value;
+
+            //Focusable = true;
+            //IsTabStop = true;
+
+            this.MouseDown += OnMouseDown;
+        }
+
+        private void OnMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Keyboard.Focus(this);
+            Focus();
+        }
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Value
+        {
+            get
+            {
+                return (string)GetValue(ValueProperty);
+            }
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        public Brush BackgroundSelected
+        {
+            get
+            {
+                return (Brush)GetValue(BackgroundSelectedProperty);
+            }
+            set
+            {
+                SetValue(BackgroundSelectedProperty, value);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
---
+++ Aronium.Wpf.Toolkit/Controls/TagsInput.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Aronium.Wpf.Toolkit.Controls
+{
+    [TemplatePart(Name = "PART_TextInput", Type = typeof(TextBox))]
+    [TemplatePart(Name = "PART_ContentPanel", Type = typeof(WrapPanel))]
+    public class TagsInput : ItemsControl
+    {
+        TextBox inputBox;
+        WrapPanel contentPanel;
+
+        static TagsInput()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TagsInput), new FrameworkPropertyMetadata(typeof(TagsInput)));
+        }
+
+        public TagsInput()
+        {
+            Focusable = false;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            inputBox = this.Template.FindName("PART_TextInput", this) as TextBox;
+            contentPanel = this.Template.FindName("PART_ContentPanel", this) as WrapPanel;
+
+            inputBox.KeyDown += OnInputBoxKeyDown;
+        }
+
+        private void OnInputBoxKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter || e.Key == System.Windows.Input.Key.Tab)
+            {
+                if (!string.IsNullOrWhiteSpace(inputBox.Text))
+                {
+                    AddTag();
+
+                    e.Handled = true;
+                }
+            }
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TagItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is TagItem;
+        }
+
+        private void AddTag()
+        {
+            this.Items.Add(inputBox.Text);
+            inputBox.Clear();
+        }
+    }
+
+}
