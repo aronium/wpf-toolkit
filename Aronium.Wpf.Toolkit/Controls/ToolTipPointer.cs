7bbd338c533dcec51bb4e820bbb8fb5d51cdcc71
Bosko Kukanjac <bosko@aronium.com>
2017-06-04 00:56:22 +0200

    ToolTipPointer position on Bottom and Left placement

--- Aronium.Wpf.Toolkit/Controls/ToolTipPointer.cs
+++ Aronium.Wpf.Toolkit/Controls/ToolTipPointer.cs
@@ -21,6 +21,7 @@ namespace Aronium.Wpf.Toolkit.Controls
 
         void OnSizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
         {
+
             switch (this.Placement)
             {
                 case PlacementMode.Left:
@@ -35,6 +36,19 @@ namespace Aronium.Wpf.Toolkit.Controls
 
                     // Arrow will be drawn on center of the tooltip content, move it up by half size
                     this.VerticalOffset = -((this.ActualHeight / 2.0) - (targetHeight / 2.0) - (_arrow.ActualHeight / 2.0) + 1.0);
+
+                    break;
+                case PlacementMode.Bottom:
+                case PlacementMode.Top:
+                    double targetWidth = 0;
+
+                    if (this.PlacementTarget is FrameworkElement)
+                    {
+                        targetWidth = (this.PlacementTarget as System.Windows.FrameworkElement).ActualWidth;
+                    }
+
+                    // Arrow will be drawn on center of the tooltip content, move it left by half size
+                    this.HorizontalOffset = -((this.ActualWidth / 2.0) - (targetWidth / 2.0) - (_arrow.ActualWidth / 2.0) + 1.0);
 
                     break;
             }
